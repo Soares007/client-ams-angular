7812507d68c5a269959d4a3af4c33037877be6c9
Soares007 <soaresstar947@gmail.com>
2023-05-09 16:38:09 -0300

    Métodos adionados

--- client-angular-ams/src/app/client.service.ts
+++ client-angular-ams/src/app/client.service.ts
@@ -7,12 +7,23 @@ import { Client } from './client';
   providedIn: 'root'
 })
 export class ClientService {
-
+  url = "http://localhost:3000/clients";
   constructor(private http: HttpClient) { }
 
-getClients(): Observable<Client[]> {
-  let url = "http://localhost:3000/clients";
-  return this.http.get<Client[]>(url);
-}
+  getClients(): Observable<Client[]> {
+    return this.http.get<Client[]>(this.url);
+  }
+
+  save(client: Client): Observable<Client>{
+  return this.http.post<Client>(this.url, client);
+  }
+
+  update(client: Client): Observable<Client>{
+    return this.http.put<Client>(`${this.url}/${client.id}`, client);
+    }
+
+  delete(client: Client): Observable<void>{
+    return this.http.delete<void>(`${this.url}/${client.id}`);
+    }
 
 }
--- client-angular-ams/src/app/clients/clients.component.ts
+++ client-angular-ams/src/app/clients/clients.component.ts
@@ -9,31 +9,70 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent implements OnInit{
+export class ClientsComponent implements OnInit {
 
-  clients: Client [] = [];
 
-  formGroupClient : FormGroup;
+  clients: Client[] = [];
+  isEditing: boolean = false;
+  formGroupClient: FormGroup;
 
-  constructor (private ClientService: ClientService,
-     private formBuilder: FormBuilder
+  constructor(private ClientService: ClientService,
+    private formBuilder: FormBuilder
 
-    ){
-       this.formGroupClient = formBuilder.group({
-         id : [''],
-         name : [''],
-         email : ['']
-       });
-    }
+  ) {
+    this.formGroupClient = formBuilder.group({
+      id: [''],
+      name: [''],
+      email: ['']
+    });
+  }
   ngOnInit(): void {
     this.loadClients();
   }
 
   loadClients() {
-   this.ClientService.getClients().subscribe( {
-    next : data => this.clients = data
-   });
+    this.ClientService.getClients().subscribe({
+      next: data => this.clients = data
+    });
   }
 
+  save() {
+    if (this.isEditing) {
+      this.ClientService.update(this.formGroupClient.value).subscribe(
+        {
+          next: () => {
+            this.loadClients();
+            this.formGroupClient.reset();
+            this.isEditing = false;
+          }
+
+        }
+      );
+    }
+    else {
+      this.ClientService.save(this.formGroupClient.value).subscribe(
+        {
+          next: data => {
+            this.clients.push(data)
+            this.formGroupClient.reset();
+
+          }
+        }
+      );
+    }
+  }
+
+  edit(client: Client) {
+    this.formGroupClient.setValue(client);
+    this.isEditing = true;
+  }
+
+  delete(client: Client) {
+    this.ClientService.delete(client).subscribe(
+      {
+        next: () => this.loadClients()
+      }
+    );
+  }
 
 }
